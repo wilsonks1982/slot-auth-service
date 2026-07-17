69030d3a387eacf2c545f689c4bea1e3fdd14d4a
Wilson Sam <wilsonks1982@gmail.com>
2026-07-17 14:38:58 +0530

    Player APIs are working - POST /api/players/session - PUT /api/players/{uid}/session/close - PUT /api/players/{uid}/pin/change - PUT /api/players/{uid}/nickname/change - GET /api/players - GET /api/players/{uid}
    Employee APIs are working
    - POST /api/employees
    - PUT /api/employees/{id}
    - GET /api/employees
    - GET /api/employees/{id}
    - DELETE /api/employees/{id}

--- src/main/java/org/wilsonks/slotauthservice/domain/Employee.java
+++ src/main/java/org/wilsonks/slotauthservice/domain/Employee.java
@@ -35,10 +35,10 @@ public class Employee {
     @Column(name = "active", nullable = false)
     private boolean active = true;
 
-    @Column(name = "updatedAt", nullable = false , updatable = false)
+    @Column(name = "created_at", nullable = false , updatable = false)
     private Instant createdAt;
 
-    @Column(name = "updatedAt", nullable = false)
+    @Column(name = "updated_at", nullable = false)
     private Instant updatedAt;
 
     @Version
